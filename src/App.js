d8e22f3495d66b28545b85e63b5fe25cfc21519b
Kauê Jacyntho <kauekaj@gmail.com>
2022-01-10 20:45:40 -0300

    create app layout

--- src/App.js
+++ src/App.js
@@ -1,21 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import foto from './assets/kaue.jpg';
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>App Test</Text>
-      <StatusBar style="auto" />
+    <>
+    <View style={styles.page}>
+      <View style={styles.container_cabecalho}>
+        <Image source={foto} style={styles.foto} />
+        <Text style={styles.nome}>KAUÊ JACYNTHO</Text>
+        <Text style={styles.funcao}>Desenvolvedor Mobile</Text>
+        <View>
+          <Text>Github</Text>
+          <Text>Facebook</Text>
+          <Text>LinkedIn</Text>
+        </View>
+      </View>
+    
     </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  page: {
+    backgroundColor: '#fefefe',
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+
+  foto: {
+    width: 225,
+    height: 225,
+    borderRadius: 110
+  },
+
+  container_cabecalho: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+
+  nome: {
+    fontSize: 26,
+    fontWeight: 'bold',
+    marginTop: 10
+  },
+
+  funcao: {
+    color: '#939393',
+    marginBottom: 10
+  }
+
 });
+
+
+export default App;
